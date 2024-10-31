c4ab9ce20cc46653cb826d544fbc4c0ff0ba7e0f
Mikul Shah <134720598+Pezerin@users.noreply.github.com>
2024-10-30 20:28:55 -0400

    Created buildtree function to assist createTree factory

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function createTree(arr = []) {
   const root = buildTree(arr);
   return { root };
 }
+
+function buildTree(arr) {
+  const sortedArray = Array.from(new Set(arr)).sort((a, b) => a - b);
+
+  function recursiveBuild(start, end) {
+    if (start > end) {
+      return null;
+    }
+
+    const mid = (start + end) / 2;
+    const root = createNode(sortedArray[mid]);
+
+    root.leftChild = recursiveBuild(start, mid);
+    root.rightChild = recursiveBuild(mid + 1, end);
+
+    return root;
+  }
+
+  return recursiveBuild(0, sortedArray.length - 1);
+}
